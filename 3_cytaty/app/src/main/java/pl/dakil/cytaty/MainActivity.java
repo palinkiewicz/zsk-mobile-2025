360846ece0fda1f8d3e24fc9bb8c75cb421f0ff4
palinkiewicz <pogromca.ap@gmail.com>
2025-10-02 07:57:36 +0200

    Complete third task

---
+++ 3_cytaty/app/src/main/java/pl/dakil/cytaty/MainActivity.java
@@ -0,0 +1,61 @@
+package pl.dakil.cytaty;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    final private String[] array = {"Dzień dobry", "Good morning", "Buenos dias"};
+    private int index = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        SeekBar bar = findViewById(R.id.seekBar);
+        TextView tx = findViewById(R.id.textView2);
+        TextView ct = findViewById(R.id.textView3);
+
+        bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                tx.setText(getString(R.string.size) + " " + i);
+                ct.setTextSize(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) { }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) { }
+        });
+
+        Button btn = findViewById(R.id.button);
+
+        ct.setText(array[index]);
+
+        btn.setOnClickListener(l -> {
+            index++;
+            if (index > array.length) index = 0;
+            ct.setText(array[index]);
+        });
+    }
+}
